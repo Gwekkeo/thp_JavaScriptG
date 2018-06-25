99901b0178278ac3f0758a9ae8763695c1d5b2aa
Gwen <gwendoline.cassand@gmail.com>
2018-06-25 22:34:57 +0200

    baisse du temps d'arrivage de la pub

--- publicite.js
+++ publicite.js
@@ -1,6 +1,6 @@
-var temps = 10000; // 10.000 = 10secondes
+var temps = 1000; // 10.000 = 10secondes
 var message = "GO VOIR CE SITE";
 
 setTimeout(function() {
 	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
-}, 10000);
+}, temps);
