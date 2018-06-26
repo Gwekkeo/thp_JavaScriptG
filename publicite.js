391fd4fa036165076ea6540aa4d38e210bf74496
Gwekkeo <39721833+Gwekkeo@users.noreply.github.com>
2018-06-26 09:50:52 +0200

    Delete publicite.js

--- publicite.js
+++
@@ -1,6 +0,0 @@
-var temps = 1000; // 10.000 = 10secondes
-var message = "GO VOIR CE SITE";
-
-setTimeout(function() {
-	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
-}, temps);
